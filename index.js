d5fb8a8a302090848a4f361f7b377d00c5fcfe05
github taylorwatsonb <github taylorwatsonb@gmail.com>
2020-05-11 08:07:06 -0400

    implemented get fetch

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("in index.js");
+const finishLine = "http://localhost:3000/api/v1/directors"
+
+document.addEventListener('DOMContentLoaded', () => {
+   getDirect()
+})
+
+function getDirect() {
+    fetch(finishLine)
+    .then(response => response.json())
+    .then(director => {
+        director.data.forEach(director => {
+            const directLayout = `
+            <div data-id=${director.id}>
+            <h3>${director.attributes.name}</h3>
+            <button data-id=${director.id}>edit</button>
+          </div>
+          <br><br>`;
+          document.querySelector('#director-container').innerHTML += directLayout;
+        })
+
+    })
+}
